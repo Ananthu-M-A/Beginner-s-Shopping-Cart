14e6e9e6cd4836426795f0aa538836f564f207d1
Ananthu M A <heartchaserananthu@gmail.com>
2023-10-10 10:59:21 +0530

    ^Ceated an EXPRESS,JS based E-COMMERCE APPLICATION with EJS...  This is my first commit to save changes.... Added both user side and admin side home page.. Listed Products in both side...

---
+++ routes/user.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  products = [
+    {
+      name: "Iphone 11",
+      brand: "Apple",
+      price: "₹50000",
+      imageAddress: "https://imgs.search.brave.com/3nywaJ5AJiOUhZe8hduAWub8XcEjEdqDhu8CWlKQRe4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzYxMmtnM3JHeVlM/LmpwZw"
+    },
+    {
+      name: "Iphone 14",
+      brand: "Apple",
+      price: "₹80000",
+      imageAddress: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-model-unselect-gallery-2-202303_GEO_US?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1676930656150"
+    },
+    {
+      name: "Iphone 13",
+      brand: "Apple",
+      price: "₹70000",
+      imageAddress: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-finish-unselect-gallery-2-202207?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1654894177855"
+    },
+    {
+      name: "Iphone 12",
+      brand: "Apple",
+      price: "₹60000",
+      imageAddress: "https://imgs.search.brave.com/3nywaJ5AJiOUhZe8hduAWub8XcEjEdqDhu8CWlKQRe4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzYxMmtnM3JHeVlM/LmpwZw"
+    },
+  ]
+  res.render('layout', { products, admin:false });
+});
+
+module.exports = router;
